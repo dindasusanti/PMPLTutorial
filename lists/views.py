680a2fd633a869dfb58754c9c95cbd8bd192dc6e
Dinda Susanti <dindaasusanti@gmail.com>
2015-09-08 14:09:12 +0000

    Commit personal webpage in localhost:8000 done

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+	return HttpResponse('<html><title>Personal Webpage</title><body><h1>Hi! Dinda Susanti<br>1206253911<h1></body></html>')
