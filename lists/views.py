f3b5c287e72d39009fe8cf70c00bf7342a05b776
Dinda Susanti <dindaasusanti@gmail.com>
2015-09-10 14:17:54 +0000

    Front page HTML now generated from a template

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return HttpResponse('<html><title>Personal Webpage</title><body><h1>Hi! Dinda Susanti<br>1206253911</h1></body></html>')
+	return render(request, 'home.html')
