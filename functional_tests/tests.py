129afa44c17c41264a884c37e2e6d1e1809373d2
Dinda Susanti <dindaasusanti@gmail.com>
2015-09-20 14:19:43 +0000

    expected error regex didn't match

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -29,6 +29,8 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -36,15 +38,34 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')		
-		self.check_for_row_in_list_table('1: Buy peacock feathers')		
-		#import time
-		#time.sleep(10)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
+		self.check_for_row_in_list_table('1: Buy peacock feathers')			
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+		
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		page_text = self.browser.find_element_by_tag_name('body')
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+		# batas tambahan
+		#table = self.browser.find_element_by_id('id_list_table')
+		#rows = table.find_elements_by_tag_name('tr')
 		#self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table -- its text was:\n%s" %(table.text,))
 		
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		#self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		#self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 		#self.fail('Finish the test!')
 
 #if __name__ == '__main__':
