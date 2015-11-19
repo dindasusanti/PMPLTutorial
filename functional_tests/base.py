5dbd15f4239c53d050c69f0e8a7f3600545056e2
Dinda Susanti <dindaasusanti@gmail.com>
2015-11-19 03:39:07 +0000

    Refactor list view to handle new item POSTs

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,6 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-import sys
+import sys 
 
 class FunctionalTest(StaticLiveServerTestCase):
 	
@@ -16,17 +16,17 @@ class FunctionalTest(StaticLiveServerTestCase):
 	@classmethod
 	def tearDownClass(cls):
 		if cls.server_url == cls.live_server_url:
-			super().tearDownClass()
+			super().tearDownClass()	
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
+		#self.browser.refresh()
 		self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
-
--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,7 @@ from lists.models import Item, List
 
 def home_page(request):
 	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
+		Item(text=request.POST['item_text'])
 		return redirect('/lists/the-only-list-in-the-world/')
 	
 	comment = 'yey, waktunya berlibur'	
@@ -22,12 +22,15 @@ def view_list(request, list_id):
 		comment = 'sibuk tapi santai'
 	else:
 		comment = 'oh tidak'
-	
+	if request.method == 'POST':
+        	Item.objects.create(text=request.POST['item_text'], list=list_)
+        	return redirect('/lists/%d/' % (list_.id,))	
+
 	return render(request, 'list.html', {'list': list_, 'comment':comment})
 
 def new_list(request):
 	list_ = List.objects.create()
-	item = Item.objects.create(text=request.POST['item_text'], list=list_)
+	item = Item(text=request.POST['item_text'], list=list_)
 	try:	
 		item.full_clean()
 		item.save()
@@ -35,9 +38,4 @@ def new_list(request):
 		list_.delete()
 		error = "You can't have an empty list item"
 		return render(request, 'home.html',{"error":error})
-	return redirect('/lists/%d/' % (list_.id,))
-
-def add_item(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	Item.objects.create(text=request.POST['item_text'], list=list_)
 	return redirect('/lists/%d/' % (list_.id,))
